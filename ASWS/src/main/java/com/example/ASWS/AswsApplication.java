2139195b49d352f88762c43a06b72b3019a19580
Keanu Hamza <66587877+Keanuhamza@users.noreply.github.com>
2021-07-29 16:00:12 +1000

    Add files via upload

---
+++ ASWS/src/main/java/com/example/ASWS/AswsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ASWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AswsApplication.class, args);
+	}
+
+}
